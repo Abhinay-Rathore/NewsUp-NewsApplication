54e3bc6d5dc9908827371529a5e9ce40b7774346
Abhinay Rathore <105166665+Abhinay-Rathore@users.noreply.github.com>
2024-01-07 17:56:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,  { useState } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 // import newsItems from './newsItems';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { HashRouter as Router, Route, Routes} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 
 
