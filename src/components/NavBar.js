2398b95c2c01f021381dfa387c88f23cc56e822f
Abhinay Rathore <105166665+Abhinay-Rathore@users.noreply.github.com>
2024-01-07 13:58:28 +0530

    package fixed 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,18 +10,18 @@ const NavBar = () =>{
     <div>
       <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <Link className="navbar-brand" to="#">NewsUp</Link>
+            <Link className="navbar-brand" to="/general">NewsUp</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <Link className="nav-link " aria-current="page" to="#">Home</Link>
+                    <Link className="nav-link " aria-current="page" to="/general">Home</Link>
                     </li> 
                    
                   <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <Link className="nav-link dropdown-toggle" to="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                       Category
                     </Link>
                     <ul className="dropdown-menu">
